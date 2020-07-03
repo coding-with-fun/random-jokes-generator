ffc7169517d18b60d19989a368ba24e661cff3d1
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-07-03 17:56:42 +0530

    This is a test
    Project Created
    
    Fixed bug
    
    Tweaking Changes
    
    Passing https request
    
    Minor tweaks
    
    Adding content message
    
    Using AXIOS for API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [joke, setJoke] = useState("Loading...");
+  const [fname, setFname] = useState("John");
+  const [lname, setLname] = useState("Doe");
+
+  const url = `https://api.icndb.com/jokes/random?firstName=${fname}&lastName=${lname}`;
+  
+  const newJoke = async () => {
+    const {
+      data: {
+        value: { joke },
+      },
+    } = await axios.get(url);
+    console.log(joke);
+    setJoke(joke);
+  };
+  
+  useEffect(() => {
+    newJoke();
+    // eslint-disable-next-line
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="AppItems">Random Jokes Generator</h2>
+      <div className="AppItems">
+        <input
+          type="text"
+          id="fname"
+          placeholder={fname}
+          onChange={(e) => setFname(e.target.value)}
+          className="AppItems"
+        />
+        <input
+          type="text"
+          id="lname"
+          placeholder={lname}
+          onChange={(e) => setLname(e.target.value)}
+          className="AppItems"
+        />
+      </div>
+      <button className="btn btn-primary AppItems" onClick={() => newJoke()}>
+        Get another joke
+      </button>
+      <h4 className="AppItems">{joke}</h4>
     </div>
   );
 }
