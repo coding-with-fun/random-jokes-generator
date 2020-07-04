7a7bf21e53551f905c5461c754fb7a6825a60b95
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-07-04 11:35:51 +0530

    Using AXIOS for API call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,17 +10,22 @@ function App() {
   const [lname, setLname] = useState("Doe");
 
   const url = `https://api.icndb.com/jokes/random?firstName=${fname}&lastName=${lname}`;
-  const newJoke = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((res2) => {
-        setJoke(res2.value.joke);
-      });
+  
+  const newJoke = async () => {
+    const {
+      data: {
+        value: { joke },
+      },
+    } = await axios.get(url);
+    console.log(joke);
+    setJoke(joke);
   };
+  
   useEffect(() => {
-    newJoke(fname, lname);
+    newJoke();
     // eslint-disable-next-line
   }, []);
+  
   return (
     <div className="App">
       <h2 className="AppItems">Random Jokes Generator</h2>
@@ -39,10 +45,7 @@ function App() {
           className="AppItems"
         />
       </div>
-      <button
-        className="btn btn-primary AppItems"
-        onClick={() => newJoke(fname, lname)}
-      >
+      <button className="btn btn-primary AppItems" onClick={() => newJoke()}>
         Get another joke
       </button>
       <h4 className="AppItems">{joke}</h4>
